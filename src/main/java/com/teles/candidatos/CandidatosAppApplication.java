4576f13b9b49b74d98e3398d05175f989642a862
Leo <hugo-pires@outlook.com>
2018-11-20 09:05:29 -0200

    project structure and dependencies

---
+++ src/main/java/com/teles/candidatos/CandidatosAppApplication.java
@@ -0,0 +1,12 @@
+package com.teles.candidatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidatosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidatosAppApplication.class, args);
+	}
+}
