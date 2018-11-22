8dc55e582c13a482dae58a5acf95da4bf692c9cb
Leo <hugo-pires@outlook.com>
2018-11-22 19:01:04 -0200

    adds zuul, fix microservices context urls both for clients and servers

--- candidaturas-api/src/main/java/com/teles/candidaturas/api/client/PessoasApiClient.java
+++ candidaturas-api/src/main/java/com/teles/candidaturas/api/client/PessoasApiClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "pessoas-api", path = "/pessoas-api")
+@FeignClient(value = "pessoas-api")
 public interface PessoasApiClient {
 
     @GetMapping("/pessoas/{id}")
--- candidaturas-api/src/main/java/com/teles/candidaturas/api/client/VagasApiClient.java
+++ candidaturas-api/src/main/java/com/teles/candidaturas/api/client/VagasApiClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "vagas-api", path = "/vagas-api")
+@FeignClient(value = "vagas-api")
 public interface VagasApiClient {
 
     @GetMapping("/vagas/{id}")
